0a284e1703091889bac1ae5de80a4a8b84a186d6
sunnyosteen <147051819+sunnyosteen@users.noreply.github.com>
2023-10-05 13:41:08 +0100

    Add files via upload

---
+++ assets/js/mywork.js
@@ -0,0 +1,15 @@
+
+
+
+alert(2+2);
+
+//Responsive Top Nav
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
